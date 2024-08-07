2c7528679670ac9f16ae72c67518db24bf129408
MoYasser <mohamed.yasser2000@ymail.com>
2024-08-07 15:05:18 +0300

    reverted changs to test CI v2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Test for CI jenkins
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
